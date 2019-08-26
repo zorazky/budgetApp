4abbcd92017662f3e76c5f99a2ae70ee20b5666b
Oswaldo <oswaldo.vazquez@gmail.com>
2019-08-26 11:22:05 -0500

    budget and expense forms working

--- js/app.js
+++ js/app.js
@@ -32,7 +32,67 @@ class UI {
         this.budgetFeedback.classList.remove("showItem")
       }, 4000)
       */
+    } else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = "";
+      this.showBalance()
     }
+  }
+
+  //show balance
+  showBalance() {
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+    if (total < 0) {
+      this.balance.classList.remove('showGreen', 'showBlack')
+      this.balance.classList.add('showRed');
+    } else if (total > 0) {
+      this.balance.classList.remove('showRed', 'showBlack')
+      this.balance.classList.add('showGreen');
+    } else if (total === 0) {
+      this.balance.classList.remove('showRed', 'showGreen')
+      this.balance.classList.add('showBlack');
+    }
+  }
+
+  //submit expense form
+  submitExpenseForm() {
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+
+    if (expenseValue === '' || amountValue === '' || amountValue < 0) {
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML = `<p>values cannot be empty or negative</p>`
+      const self = this;
+      setTimeout(function() {
+        self.expenseFeedback.classList.remove("showItem")
+      }, 4000)
+    } else {
+      let amount = parseInt(amountValue)
+      this.expenseInput = ''
+      this.amountInput = ''
+
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount:amount
+      }
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      // show balance
+    }
+  }
+  //add expense
+  addExpense(expense) {
+    
+  }
+
+  //total expense
+  totalExpense() {
+    let total = 400;
+    return total;
   }
 }
 
@@ -53,6 +113,7 @@ function eventListeners() {
    // expense form submit
    expenseForm.addEventListener('submit', function(event) {
     event.preventDefault();
+    ui.submitExpenseForm();
   })
 
    // expense click
